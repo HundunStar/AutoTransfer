7dea6ba88641a1d4ae0fa60a2be433a66f3a25cc
HundunStar <648377563@qq.com>
2015-09-26 18:07:09 +0800

    更新主类，添加传送轨道（未完成）

--- main/java/com/BioStace/AutoTransfer/AutoTransfer.java
+++ main/java/com/BioStace/AutoTransfer/AutoTransfer.java
@@ -1,12 +1,28 @@
 package com.BioStace.AutoTransfer;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 
 @Mod(modid=AutoTransfer.MODID,name=AutoTransfer.NAME,version=AutoTransfer.Version)
 public class AutoTransfer {
 
-	public static final String MODID="biomod";
-	public static final String NAME="BioChemistryCraft";
-	public static final String Version="1.0.0";
+	public static final String MODID="AutoTransMod";
+	public static final String NAME="AutoTransfer";
+	public static final String Version="1.0.1";
+	
+	@EventHandler
+	public void preLoad(FMLPreInitializationEvent event){
+	}
+
+	@EventHandler
+	public void load(FMLInitializationEvent event){
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+	}
 }
---
+++ main/java/com/BioStace/AutoTransfer/Block/BlockTransOrbit.java
@@ -0,0 +1,5 @@
+package com.BioStace.AutoTransfer.Block;
+
+public class BlockTransOrbit {
+
+}
