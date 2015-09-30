25b48008a0b43791a75f65a9230d1643b24a1dbd
KAAAsS <guyishao@foxmail.com>
2015-09-30 20:31:44 +0800

    fixes conflicts

--- main/java/com/BioStace/AutoTransfer/Registry/BlockRegistry.java
+++ main/java/com/BioStace/AutoTransfer/Registry/BlockRegistry.java
@@ -1,19 +1,19 @@
 package com.BioStace.AutoTransfer.Registry;
 
-import com.BioStace.AutoTransfer.Block.*;
+import com.BioStace.AutoTransfer.Block.BlockTransOrbit;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 
 public class BlockRegistry {
-	public static Block transorbit;
-	public BlockRegistry()
-	{
-		transorbit=new BlockTransOrbit(Material.iron);
+	public static Block transOrbit;
+	
+	public static void init () {
+		transOrbit = new BlockTransOrbit();
+		registry();
 	}
-	public void init()
-	{
-		GameRegistry.registerBlock(transorbit, "TransferOrbit");
+	
+	public static void registry () {
+		GameRegistry.registerBlock(transOrbit, "transOrbit");
 	}
 }
